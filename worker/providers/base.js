4f89e5c29e7237c56e0312d8ff82bb24f606f3c5
shawnamika-prog <shawnamika@gmail.com>
2026-08-18 21:01:23 +0200

    Delete worker directory

--- worker/providers/base.js
+++
@@ -1,4 +0,0 @@
-export class TTSProvider {
-  constructor(env) { this.env = env; }
-  async generate() { throw new Error("Provider not implemented"); }
-}
--- worker/providers/index.js
+++
@@ -1,7 +0,0 @@
-import { DeepgramProvider } from "./deepgram.js";
-export function getProvider(env, voice) {
-  switch ((voice.provider || "deepgram").toLowerCase()) {
-    case "deepgram": return new DeepgramProvider(env);
-    default: throw new Error(`Unsupported provider: ${voice.provider}`);
-  }
-}
