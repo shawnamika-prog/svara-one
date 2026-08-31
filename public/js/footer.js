cd19c87fecc1221dd3f11c4b7e45ef57097861ad
shawnamika-prog <shawnamika@gmail.com>
2026-08-31 18:49:07 +0200

    Link SvaraFlow footer item to Hero SvaraFlow section

--- public/js/footer.js
+++ public/js/footer.js
@@ -27,7 +27,7 @@
           <p>Engineered Intelligence.<br>Human Orchestration.</p>
         </div>
         <div class="svara-footer-links">
-          <div><h3>PRODUCT</h3><a href="/studio">Voice Studio</a><span>SvaraFlow™</span><a href="/#workflow">How it works</a><a href="/#pricing">Pricing</a></div>
+          <div><h3>PRODUCT</h3><a href="/studio">Voice Studio</a><a href="/#svaraflow">SvaraFlow™</a><a href="/#workflow">How it works</a><a href="/#pricing">Pricing</a></div>
           <div><h3>ACCOUNT</h3><a href="/login.html">Sign in</a><a href="/signup.html">Get started</a></div>
           <div><h3>COMPANY</h3><a href="/company.html#about">About SvaraONE</a><a href="/company.html#vision">Vision</a><a href="/company.html#mission">Mission</a></div>
           <div><h3>SUPPORT &amp; LEGAL</h3><span>Help Centre</span><span>Contact Support</span><span>Terms of Use</span><span>Privacy Policy</span></div>
